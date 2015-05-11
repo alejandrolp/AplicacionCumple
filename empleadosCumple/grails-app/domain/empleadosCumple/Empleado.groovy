4c85379564e32a119914e2fd96a6dfeba1e08f7b
jbordes <juanpablo.bordes@gmail.com>
2015-05-11 14:51:11 -0300

    Para mergear Empleado

--- empleadosCumple/grails-app/domain/empleadosCumple/Empleado.groovy
+++ empleadosCumple/grails-app/domain/empleadosCumple/Empleado.groovy
@@ -11,13 +11,14 @@ class Empleado {
 	Empresa empresa /* UNA empresa <posee> MUCHOS empleados */
 	User user
 
-	static hasMany = [Regalo]
+	static hasMany = [regalos:Regalo]
 
     static constraints = {    	
 		nombre nullable:false
 		apellido nullable:false
 		fechaCumple nullable:false
 		legajo nullable:false
+		user nullable:true
     }
 
 }
